2e2913cead439e07094fb35f9a33b4ccfb0e9ba2
Mattias Ottosson <himself@mattiasottosson.se>
2013-05-31 21:40:33 +0200

    Make js object orientated

--- js/main.js
+++ js/main.js
@@ -1,58 +1,58 @@
 /*This is the url we are on.*/
-var thisurl = document.URL;
+var underlayer = {
+  init: function() {
+    this.url = document.URL;
+    this.underlay();
 
-$( document ).ready(function() {
-  $(document.body).on('change', '#bgfile', addimage);
+    /* check if current url has an image assosiated with it – otherwise, prompt user to upload image */
+    typeof(localStorage[this.url] === "undefined") ? this.setImage() : this.showImageDialog();
+  },
 
+  addImage: function(evt) {
+    var file = evt.target.files[0];
+    /* Only process image files.*/
+    if(file.type.match('image.*')) {
+      /*Add image to FileReader object*/
+      var reader = new FileReader();
+      reader.readAsDataURL(file);
+      /*Add image to local storage */
+      reader.onload = (function(theFile) {
+        return function(e) {
+          localStorage.setItem(underlayer.url, e.target.result);
+          /*Add image to background*/
+          underlayer.hideImageDialog();
+          underlayer.setImage();
+        };
+      })(file);
+    }
+  },
 
-  /*check if current url has an image assosiated with it.*/
-  if(localStorage[thisurl]){
-    /*if it has, underlay that image*/
-    underlay();
-    setimage();
+  showImageDialog: function(){
+    var $imageDialog = $('<div id="imagedialog"></div>'),
+        $imageInput = $('<input type="file" id="bgfile" name="files[]" style="position: absolute; top: 50%; left: 50%; margin: -50px 0 0 -150px; background: gray; width: 260px; height: 60px; padding: 20px; z-index:1;" />');
+
+    $('body').prepend($imageDialog.append($imageInput));
+  },
+
+  hideImageDialog: function(){
+    $('#imagedialog').remove();
+  },
+
+  underlay: function(){
+    /*make page transparent*/
+    $('body > *').css('opacity', 0.5);
+    /*Add image behind*/
+    $('body').prepend('<div id="underlayer" style="width: 100%; height: 100%; position: absolute; background-repeat: no-repeat; background-position: center top; top: 0;" />');
+  },
+
+  setImage: function(){
+    var imgData = localStorage.getItem(this.url);
+    $('#underlayer').css('background-image','url("'+imgData+'")')
   }
-  else{
-    /*otherwise, prompt user to upload image*/
-    underlay();
-    showimagedialog();
-  }
 
+}
+
+$(function() {
+  underlayer.init();
+  $(document.body).on('change', '#bgfile', underlayer.addImage);
 });
-
-
-function addimage(evt) {
-  var file = evt.target.files[0]; 
-  /* Only process image files.*/
-  if (file.type.match('image.*')) {
-    /*Add image to FileReader object*/
-    var reader = new FileReader();
-    reader.readAsDataURL(file);
-    /*Add image to local storage */
-    reader.onload = (function(theFile) {
-      return function(e) {
-        localStorage.setItem(thisurl, e.target.result);
-        /*Add image to background*/
-        hideimagedialog();
-        setimage();
-      };
-    })(file);
-  }
-}
-
-
-function showimagedialog(){
-  $('body').prepend('<div id="imagedialog"><input type="file" id="bgfile" name="files[]" style="position: absolute;top: 50%;left: 50%;margin: -50px 0 0 -150px;background: gray;width: 260px;height: 60px; padding: 20px;z-index:1;" />');
-}
-function hideimagedialog(){
-  $('#imagedialog').remove();
-}
-function underlay(){
-  /*make page transparent*/
-  $('body > *').css('opacity', 0.5);
-  /*Add image behind*/
-  $('body').prepend('<div id="underlayer" style="width: 100%;height: 100%;position: absolute;background-repeat: no-repeat;background-position: center top;top: 0;" />')
-}
-function setimage(){
-  var imgurl = localStorage.getItem(thisurl)
-  $('#underlayer').css('background-image','url("'+imgurl+'")')
-}
