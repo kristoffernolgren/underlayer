d9ba226df74c5a21b997c0b8da8253e82cc4438e
Kristoffer Nolgren <kristoffer@nolgren.se>
2013-06-02 21:41:00 +0200

    dialog never disappeared.

--- js/underlayer.js
+++ js/underlayer.js
@@ -1,10 +1,9 @@
 //todo add a namespace to all local-storage-variables
-
+//Image disapears when toggling on/off
 var underlayer = {
   init: function() {
     this.url = document.URL;
     this.setListeners();
-    //Gör en toggler för dialog-rutan också. Den ska bara funka när underlayer-toggled är på.
     //Knappar till dialog-rutan istället för fält.
     //lägg bilden längst ner i skissen dom:en istället så den inte är så ivägen.
 
@@ -12,7 +11,9 @@ var underlayer = {
     if(localStorage.getItem('underlayer-toggled') != 'false') {
       this.showUnderlay();
       //show image-dialog if no image is set
-      if(typeof(localStorage[this.url] === "undefined")){
+
+
+      if(localStorage.getItem([this.url]) === null){
         localStorage.setItem('underlayer-toggled',true)
         this.showDialog();
       }
@@ -31,7 +32,7 @@ var underlayer = {
         underlayer.toggle();
         break;
       case 85:
-        underlayer.showDialog();
+          underlayer.showDialog();
         break;
       default:
         return;
@@ -54,30 +55,34 @@ var underlayer = {
     } else {
       localStorage.setItem('underlayer-toggled',true)
       this.showUnderlay();
-      this.showDialog();
+      if(localStorage.getItem([this.url]) === null){
+        this.showDialog();
+      };
     }
   },
 
 
   showDialog: function(){
-    var $Dialog = $('<div id="Dialog" style="position: absolute;top: 50%;left: 50%;margin: -50px 0 0 -150px;background: gray;width: 300px;height: 150px;padding: 20px"></div>'),
-        $imageInput = $('<input type="file" id="bgfile" name="files[]" style="margin-bottom:5px;" />'),
-        $inputTop = $('<input type="text" class="bg-position" placeholder="top" id="bg-position-top" style="border: 1px solid black;margin-bottom:5px;" />'),
-        $inputLeft = $('<input type="text" class="bg-position" placeholder="left" id="bg-position-left" style="border: 1px solid black;margin-bottom:5px;" />'),
-        $closeButton = $('<a style="display: block;" id="closeButton" href="#">Save &amp; Close</a>');
-    $('body').append($Dialog.append($imageInput, $inputTop, $inputLeft, $closeButton));
+    if(!($('#dialog').length)){
+      var $dialog = $('<div id="dialog" style="position: absolute;top: 50%;left: 50%;margin: -50px 0 0 -150px;background: gray;width: 300px;height: 150px;padding: 20px"></div>'),
+          $imageInput = $('<input type="file" id="bgfile" name="files[]" style="margin-bottom:5px;" />'),
+          $inputTop = $('<input type="text" class="bg-position" placeholder="top" id="bg-position-top" style="border: 1px solid black;margin-bottom:5px;" />'),
+          $inputLeft = $('<input type="text" class="bg-position" placeholder="left" id="bg-position-left" style="border: 1px solid black;margin-bottom:5px;" />'),
+          $closeButton = $('<a style="display: block;" id="closeButton" href="#">Save &amp; Close</a>'),
+          $instruction = $('<p style="font-size: 11px;color: black;text-shadow: 0 0 1.3em white;margin-left: 1em;">Press "U" to Update your image-settings later.</p><p style="font-size: 11px;color: black;text-shadow: 0 0 1.3em white;margin-left: 1em;">Press "T" to Toggle on/off.</p>');
+      $('body').append($dialog.append($imageInput, $inputTop, $inputLeft, $closeButton, $instruction));
+      };
   },
 
   hideDialog: function(){
-    $('#Dialog').remove();
+    $('#dialog').remove();
   },
 
   showUnderlay: function(){
     /*make page transparent*/
-    $('body > *').css('opacity', 0.5);
+    $('body > *:not(#dialog)').css('opacity', 0.5);
     /*Add image behind*/
-    $('body').prepend('<div id="underlayer" style="width: 100%; height: 100%; position: absolute; background-repeat: no-repeat; background-position: center top; top: 0;" ><p style="font-size: 11px;color: white;text-shadow: 0 0 1.3em black;margin-left: 1em;">Press "U" to Update your image-settings.</p></div>');
-    typeof(localStorage[this.url] === "undefined") ? this.setImage() : this.showDialog();
+    $('body').prepend('<div id="underlayer" style="width: 100%; height: 100%; position: absolute; background-repeat: no-repeat; background-position: center top; top: 0;" ></div>');
     this.setPosition();
   },
 
