1860abc8ee36884eb7fde1f4845aef1137d86ec5
Kristoffer Nolgren <kristoffer@nolgren.se>
2013-06-19 13:34:11 +0200

    spaces to tabs

--- js/underlayer.js
+++ js/underlayer.js
@@ -1,142 +1,187 @@
 //todo add a namespace to all local-storage-variables
 var underlayer = {
-  init: function() {
-    this.url = document.URL;
-    this.setListeners();
-    //lägg bilden längst ner i skissen dom:en istället så den inte är så ivägen.
+	init: function() {
+		this.url = document.URL;
+		this.setListeners();
 
-    //gör det här med togglern istället kanske?
-    if(localStorage.getItem('underlayer-toggled') != 'false') {
-      this.showUnderlay();
-
-      //show image-dialog if no image is set
-      if(localStorage.getItem([this.url]) === null){
-        localStorage.setItem('underlayer-toggled',true)
-        this.showDialog();
-      }
-    }
-  },
-
-  setListeners: function() {
-    $(document).keydown(function(e) {
-      var key = e.keyCode;
-      switch(key) {
-        case 84:
-          underlayer.toggle();
-          break;
-        case 85:
-            underlayer.showDialog();
-          break;
-        default:
-          return;
-      }
-    });
-
-    $(document).keyup(function(e) {
-      var key = e.keyCode;
-
-      if(key == 91) {
-        underlayer.cmdpressed = false;
-      }
-    });
-  },
-
-  toggle: function() {
-    if(localStorage.getItem('underlayer-toggled') == 'true') {
-     this.hideUnderlay();
-    } else {
-      this.showUnderlay();
-      if(localStorage.getItem([this.url]) === null){
-        this.showDialog();
-      };
-    }
-  },
+		localStorage.setItem('offsetX', 0)
+		localStorage.setItem('offsetY', 0)
 
 
-  showDialog: function(){
-    if(localStorage.getItem('underlayer-toggled') == 'false') {
-      this.showUnderlay();
-    };
-    if(!($('#dialog').length)){
-      var $dialog = $('<div id="dialog" style="position: absolute;top: 50%;left: 50%;margin: -50px 0 0 -150px;background: gray;width: 300px;height: 150px;padding: 20px"></div>'),
-          $imageInput = $('<input type="file" id="bgfile" name="files[]" style="margin-bottom:5px;" />'),
-          $positionButtons = $('<a class="align" data-postion="left" href>Left</a><a class="align" data-postion="center" href>Center</a><a class="align" data-postion="right" href>Right</a>')
-          $closeButton = $('<a style="display: block;" id="closeButton" href>Save &amp; Close</a>'),
-          $instruction = $('<p style="font-size: 11px;color: black;text-shadow: 0 0 1.3em white;margin-left: 1em;">Press "U" to Update your image-settings later.</p><p style="font-size: 11px;color: black;text-shadow: 0 0 1.3em white;margin-left: 1em;">Press "T" to Toggle on/off.</p><p style="font-size: 11px;color: black;text-shadow: 0 0 1.3em white;margin-left: 1em;">Use arrows to offset(not implemnted yet).</p>');
-      $('body').append($dialog.append($imageInput, $positionButtons, $closeButton, $instruction));
-      };
-  },
 
-  hideDialog: function(){
-    $('#dialog').remove();
-  },
+		//gör det här med toggler-funktionen istället kanske?
+		if(localStorage.getItem('underlayer-toggled') != 'false') {
+			this.showUnderlay();
 
-  showUnderlay: function(){
-    localStorage.setItem('underlayer-toggled',true);
-    /*make page transparent*/
-    $('body > *:not(#dialog)').css('opacity', 0.5);
-    /*Add image behind*/
-    $('body').prepend('<div id="underlayer" style="width: 100%; height: 100%; position: absolute; background-repeat: no-repeat; background-position: center top; top: 0;" ></div>');
-    this.setImage();
-    this.setPosition();
+			//show image-dialog if no image is set
+			if(localStorage.getItem([this.url]) === null){
+				localStorage.setItem('underlayer-toggled',true)
+				this.showDialog();
+			}
+		}
+	},
 
-  },
+	setListeners: function() {
+		$(document).keydown(function(e) {
+			var key = e.keyCode;
+			switch(key) {
+				case 84:
+					underlayer.toggle();
+					break;
+				case 85:
+					underlayer.showDialog();
+					break;
 
-  hideUnderlay: function() {
-    localStorage.setItem('underlayer-toggled',false);
-    $('body > *').css('opacity', '');
-    $('#underlayer').remove();
-    $('#position-dialog').remove();
-    this.hideDialog();
-  },
+				case 37:
+					if(localStorage.getItem('underlayer-toggled') == 'true') {
+						e.preventDefault();
+						underlayer.backgroundOffset('left');
+						break;
+					}
+				case 38:
+					if(localStorage.getItem('underlayer-toggled') == 'true') {
+						e.preventDefault();
+						underlayer.backgroundOffset('up');
+						break;
+					}
+				case 39:
+					if(localStorage.getItem('underlayer-toggled') == 'true') {
+						e.preventDefault();
+						underlayer.backgroundOffset('right');
+						break;
+					}
+				case 40:
+					if(localStorage.getItem('underlayer-toggled') == 'true') {
+						e.preventDefault();
+						underlayer.backgroundOffset('down');
+						break;
+					}
+				default:
+					return;
+			}
+		});
+	},
 
-  addImage: function(evt) {
-    var file = evt.target.files[0];
-    /* Only process image files.*/
-    if(file.type.match('image.*')) {
-      /*Add image to FileReader object*/
-      var reader = new FileReader();
-      reader.readAsDataURL(file);
-      /*Add image to local storage */
-      reader.onload = (function(theFile) {
-        return function(e) {
-          localStorage.setItem(underlayer.url, e.target.result);
-          //add image
-          underlayer.setImage();
-        };
-      })(file);
-    }
-  },
+	backgroundOffset: function(direction) {
+		var offsetX = parseInt(localStorage.getItem('offsetX'));
+		var offsetY = parseInt(localStorage.getItem('offsetY'));
+		switch(direction){
+			case 'left':
+				localStorage.setItem('offsetY',offsetY-1);
+				break;
+			case 'right':
+				localStorage.setItem('offsetY',offsetY+1);
+				break;
+			case 'up':
+				localStorage.setItem('offsetX',offsetX-1);
+				break;
+			case 'down':
+				localStorage.setItem('offsetX',offsetX+1);
+				break;
+			default:
+				return;
+		}
+		$('#underlayer').css({
+			'margin-left': offsetY,
+			'margin-top': offsetX
+		});
+	},
 
-  addPosition: function(alignment){
-    localStorage.setItem('alignment', alignment);
-    underlayer.setPosition();
-  },
+	toggle: function() {
+		if(localStorage.getItem('underlayer-toggled') == 'true') {
+		 this.hideUnderlay();
+		} else {
+			this.showUnderlay();
+			if(localStorage.getItem([this.url]) === null){
+				this.showDialog();
+			};
+		}
+	},
 
-  setImage: function(){
-    var imgData = localStorage.getItem(this.url);
-    $('#underlayer').css('background-image','url("'+imgData+'")')
-  },
 
-  setPosition: function() {
-    var posData = localStorage.getItem('alignment');
-    console.log(posData)
-    $('#underlayer').css('background-position','top '+ posData)
-  },
+	showDialog: function(){
+		if(localStorage.getItem('underlayer-toggled') == 'false') {
+			this.showUnderlay();
+		};
+		if(!($('#dialog').length)){
+			var $dialog = $('<div id="dialog" style="position: absolute;top: 50%;left: 50%;margin: -50px 0 0 -150px;background: gray;width: 300px;height: 150px;padding: 20px"></div>'),
+					$imageInput = $('<input type="file" id="bgfile" name="files[]" style="margin-bottom:5px;" />'),
+					$positionButtons = $('<a class="align" data-postion="left" href>Left</a><a class="align" data-postion="center" href>Center</a><a class="align" data-postion="right" href>Right</a>')
+					$closeButton = $('<a style="display: block;" id="closeButton" href>Save &amp; Close</a>'),
+					$instruction = $('<p style="font-size: 11px;color: black;text-shadow: 0 0 1.3em white;margin-left: 1em;">Press "U" to Update your image-settings later.</p><p style="font-size: 11px;color: black;text-shadow: 0 0 1.3em white;margin-left: 1em;">Press "T" to Toggle on/off.</p><p style="font-size: 11px;color: black;text-shadow: 0 0 1.3em white;margin-left: 1em;">Use arrows to offset(not implemnted yet).</p>');
+			$('body').append($dialog.append($imageInput, $positionButtons, $closeButton, $instruction));
+			};
+	},
+
+	hideDialog: function(){
+		$('#dialog').remove();
+	},
+
+	showUnderlay: function(){
+		localStorage.setItem('underlayer-toggled',true);
+		/*make page transparent*/
+		$('body > *:not(#dialog)').css('opacity', 0.5);
+		/*Add image behind*/
+		$('body').append('<div id="underlayer" style="width: 100%; height: 100%; position: absolute; background-repeat: no-repeat; background-position: center top; top: 0;" ></div>');
+		this.setImage();
+		this.setPosition();
+
+	},
+
+	hideUnderlay: function() {
+		localStorage.setItem('underlayer-toggled',false);
+		$('body > *').css('opacity', '');
+		$('#underlayer').remove();
+		$('#position-dialog').remove();
+		this.hideDialog();
+	},
+
+	addImage: function(evt) {
+		var file = evt.target.files[0];
+		/* Only process image files.*/
+		if(file.type.match('image.*')) {
+			/*Add image to FileReader object*/
+			var reader = new FileReader();
+			reader.readAsDataURL(file);
+			/*Add image to local storage */
+			reader.onload = (function(theFile) {
+				return function(e) {
+					localStorage.setItem(underlayer.url, e.target.result);
+					//add image
+					underlayer.setImage();
+				};
+			})(file);
+		}
+	},
+
+	addPosition: function(alignment){
+		localStorage.setItem('alignment', alignment);
+		underlayer.setPosition();
+	},
+
+	setImage: function(){
+		var imgData = localStorage.getItem(this.url);
+		$('#underlayer').css('background-image','url("'+imgData+'")')
+	},
+
+	setPosition: function() {
+		var posData = localStorage.getItem('alignment');
+		$('#underlayer').css('background-position','top '+ posData)
+	},
 
 
 }
 
 $(function() {
-  underlayer.init();
-  $(document.body).on('change', '#bgfile', underlayer.addImage);
-  $(document.body).on('click', '#closeButton', function(){
-    event.preventDefault();
-    underlayer.hideDialog();
-  });
-  $(document.body).on('click', '.align', function(){
-    event.preventDefault();
+	underlayer.init();
+	$(document.body).on('change', '#bgfile', underlayer.addImage);
+	$(document.body).on('click', '#closeButton', function(){
+		event.preventDefault();
+		underlayer.hideDialog();
+	});
+	$(document.body).on('click', '.align', function(){
+		event.preventDefault();
 
-    underlayer.addPosition($(this).attr('data-postion'))
-  });
+		underlayer.addPosition($(this).attr('data-postion'))
+	});
 });
